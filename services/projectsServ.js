3e5644aa3690aca8967769c24a724318980dd1c8
Abosede Alli <aoall@deakin.edu.au>
2022-01-15 07:38:12 +1100

    updated io, getprojectby ID

--- services/projectsServ.js
+++ services/projectsServ.js
@@ -5,22 +5,34 @@ let dbo = require("../database/db");
     setTimeout(()=> {
         projectCollection=dbo.getDb().collection('projects');}
     ,2000);
+    
+
     const getAllProjects = (response)=>{
-        projectCollection.find().toArray((err, result) => { if (err)throw err;
-response.send(result);        })
+    projectCollection.find().toArray((err, result) => { if (err)throw err;
+response.send(result);        
+    });
 }
 
-const insertProject=(project,response)=>{
+const getProjectByID = (id, response)=>{
+    console.log(id);
+    projectCollection.find({projectID: id}).toArray((err, result) => { if (err)throw err;
+response.send(result);        
+    });
+
+const insertProject=(project,response, io)=>{
     projectCollection.insertOne(project, (err, result) => { if (err)throw err;
+        //transmitting message as new user is inserted
+        io.emit("project:update", project);      
         response.send({result:204}); 
     });
 }
 const deleteProject = (id,response) => {
     
-        projectCollection.deleteOne({ projectID: id }, (err, result) => {
-            if (err) throw err;
-            response.send({ result: 204 });
-        })
+    projectCollection.deleteOne({ projectID: id }, (err, result) => {
+    if (err) throw err;
+    response.send({ result: 204 });
+    });
 }
-module.exports= { getAllProjects, insertProject, deleteProject 
+
+module.exports= { getAllProjects, getProjectByID, insertProject, deleteProject 
 }
