d51e47a8c543a11b6974d9091c17be18b16808b6
Abosede Alli <aoall@deakin.edu.au>
2022-02-18 10:31:29 +1100

    updated io, getprojectby ID

--- controller/projectsControl.js
+++ controller/projectsControl.js
@@ -1,7 +1,10 @@
 let service = require("../services/projectsServ");
 const getProjects=(response)=>{
-    service.getAllProjects(response);
+    service.getProjects(response);
 
+}
+const getAllProjects=(response)=>{
+    service.getAllProjects(response);
 }
 const insertProject=(project, response)=>{
     service.insertProject(project, response); }
@@ -10,4 +13,4 @@ const deleteProject=(id, response)=>{
     service.deleteProject(id, response); } 
     
     
-module.exports={ getProjects, insertProject, deleteProject }
+module.exports={ getProjects, insertProject, deleteProject, getAllProjects }
